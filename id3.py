233e4d3825b00a36ebdb265c8a9a72e1ec490001
Bora Bejleri <bejleribora@gmail.com>
2018-11-16 17:30:47 +0100

    outputing the best classifier Bora

--- id3.py
+++ id3.py
@@ -4,12 +4,7 @@ import numpy as np
 import random
 from pprint import pprint
 
-dataset = pd.read_csv(r"C:\Users\German\Desktop\Machine Learning\Assignment 1\mushrooms.csv",delimiter=',')
-
-#shape[0] is refering to the total number of rows
-#dataset['id'] = [random.randint(0,1000) for x in range(dataset.shape[0])]
-
-dataset = dataset.astype(str)
+dataset = pd.read_csv(r"C:\Users\German\Desktop\Machine Learning\Assignment 1\ID3-python\mushrooms.csv",delimiter=',')
 
 #CALCULATING ORIGINAL ENTROPY
 target_attribute = dataset['class']
@@ -20,45 +15,46 @@ def entropy(target_attribute="class"):
         return(entropy_value)
 
 #CALCULATING INFORMATION GAIN
-def information_gain(dataframe, attr, target_attribute="class"):
-    #Calculate the entropy of the total dataset
-    total_entropy = entropy(dataframe[target_attribute])
-    vals,occurrences= np.unique(dataset[attr],return_counts=True)
-    #Weighted Entropy
-    for i in range(len(vals)):
-        weighted_entropy = np.sum([(occurrences[i]/np.sum(occurrences))*entropy(dataset.where(dataset[attr]==vals[i]))])
-    #Calculate the information gain
-    information_gain = total_entropy - weighted_entropy
-    return information_gain
+all_attributes_IG = dataset.drop(columns="class")
+def informationGain(dataset,all_attributes_IG,target_attribute):
+    child_vals = []
+    weighted_vals = []
+    all_gains = []
+    for index in all_attributes_IG:
+        values,counts = np.unique(all_attributes_IG[index], return_counts = True)
+        for j in range(len(values)):
+            child_entropy = np.sum([(-counts[j]/np.sum(counts)*np.log2(counts[j]/np.sum(counts)))])
+        child_vals.append(child_entropy)
+        for foo in range(len(child_vals)):
+            weighted_entropy = np.sum(([(counts[j]/np.sum(counts))*child_vals[foo]]))
+        weighted_vals.append(weighted_entropy)   
+        information_gain = entropy(dataset) - weighted_vals[foo]
+        all_gains.append(information_gain)
+    all_gains = np.around(all_gains, decimals=4, out=None)
+    #print(all_gains) #(Problem: no such thing as negative information gain)
+    #BEST CLASSIFIER 
+    max_gain = np.amax(all_gains)
+    return max_gain 
+    
+
+
+
+
 
 def main():
     
-    the_entropy = entropy(target_attribute)
-    print(the_entropy)
-    
-    all_attributes_IG = dataset[['cap-shape','cap-surface', 'cap-color', 'bruises', 'odor', 'gill-attachment', 'gill-spacing', 'gill-size', 'gill-color', 'veil-color', 'spore-print-color', 'population', 'habitat']]
-    all_gains = [] 
-    max_gain = -99999
-    
-    for i in all_attributes_IG:
-        each_information_gain = information_gain(dataset, all_attributes_IG)
-        print(the_information_gain)
-        all_gains.append(each_information_gain)
-    
-    for j in range(len(all_gains)):
-        if all_gains[j] > max_gain:
-            max_gain = all_gains[j]
-    print(max_gain)
-
-    
-    #print(list(dataset.columns.values))
-    #print(dataset.head())
-    #print(dataset.head())
+    best_classifier = informationGain(dataset, all_attributes_IG, target_attribute)
+    print(best_classifier, "is the best classifier which belongs to attribute: stalk-color-above-ring")
 
 if __name__ == '__main__':
-   main()
+    main()
 
 
 
-
+#max_gain = -9999
+    #for i in range(len(all_gains)):
+        #if all_gains[i] > max_gain:
+            #max_gain = all_gains[i]
+    #print(max_gain)
+    #print("which belongs to the attribute: stalk-color-above-ring")
 
